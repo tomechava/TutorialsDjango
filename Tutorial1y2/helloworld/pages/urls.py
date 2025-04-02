84dcdf5525e49b3652c8cd5e4adec80378db816a
tomechava <techavarrg@eafit.edu.co>
2025-04-02 10:58:39 -0500

    Tutorial dependencias finished

--- Tutorial1y2/helloworld/pages/urls.py
+++ Tutorial1y2/helloworld/pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .utils import ImageLocalStorage
 from .views import (
     HomePageView,
     AboutPageView,
@@ -9,6 +10,7 @@ from .views import (
     SuccessPageView,
     CartView,
     CartRemoveAllView,
+    ImageViewFactory,
 )
 
 
@@ -22,5 +24,7 @@ urlpatterns = [
     path("products/<str:id>", ProductShowView.as_view(), name="show"),
     path('cart/', CartView.as_view(), name='cart_index'),
     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
 ]
